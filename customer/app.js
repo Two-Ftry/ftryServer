0fc31c4117d89d047e52e600db0a22955c82eead
suifengfengye <1527261080@qq.com>
2016-09-19 22:12:14 +0800

    tijiao

---
+++ customer/app.js
@@ -0,0 +1,21 @@
+/**
+ * @desc 入口文件
+ * @require
+ * @author jianfeng_huang.
+ * @date 2016/9/19.
+ */
+var express = require('express');
+var app = express();
+
+//app.get('/', function(req, res){
+//    res.send('hello ftry customer');
+//});
+
+//var server = app.listen(7654, function(){
+//    var host = server.address().address;
+//    var port = server.address().port;
+//    console.log('ftry customer app listening at http://%s:%s', host, port);
+//});
+
+
+module.exports = app;
