7e28c41a94363ffb5b41f133c199b7b4a043868c
jianfeng_huang <jianfeng_huang@kingdee.com>
2016-09-21 17:39:59 +0800

    submit

--- customer/app.js
+++ customer/app.js
@@ -6,36 +6,51 @@
  */
 var express = require('express');
 var app = express();
+
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var URL = require('url');
+var logger = require('morgan');
+var fs = require('fs');
+var FileStreamRotator = require('file-stream-rotator');
 
 app.use(favicon(__dirname + '/assests/img/bitbug_favicon.ico'));
 
 //解析json数据 application/json
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 //解析 application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({extended: false}));
-var bpurlencoded = bodyParser.urlencoded({extended: false});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', bpurlencoded, function(req, res){
+app.use(cookieParser());
+
+//记日志
+//app.use(logger('dev'));//:method :url :status :response-item ms - :res[conent-length]
+var logDirectory = __dirname + '/log';
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+var accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: logDirectory + '/access-%DATE%.log',
+    frequency: 'daily',
+    verbose: false
+});
+app.use(logger('combined', {stream: accessLogStream}));
+
+app.post('/', function(req, res){
     console.log(req.body);
-    res.send('hello serve favicon2!!!:' + req.body.a1 + '-' + req.body.a2);
+    res.end('hello serve favicon2!!!:' + req.body.a1 + '-' + req.body.a2);
 });
 
-app.post('/login', bpurlencoded, function(req, res){
+app.post('/login', function(req, res){
     console.log('login post', req.body);
-    //res.send('login2:' + req.body.a1 + '-' + req.body.a2);
-    //res.setData({
-    //    r1: 'r111'
-    //});
+    res.sendData({a: 'fy'});
+    res.end('login post ok');
 });
-app.get('/login', bpurlencoded, function(req, res){
-    console.log('login get', req.body);
-    //res.send('login2:' + req.body.a1 + '-' + req.body.a2);
-    //res.setData({
-    //    r1: 'r111'
-    //});
+app.get('/login', function(req, res){
+    var url = URL.parse(req.url, true);
+    console.log('login get', url.query, url.hash);
+    res.end('login get ok hhh');
 });
 
 module.exports = app;
